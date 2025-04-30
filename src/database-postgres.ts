72bd0fc05a111bed749e80aa437ad3690eeb9b30
dinihz <lucas87diniz@gmail.com>
2025-04-30 20:54:03 -0300

    feat: add database-postgres and endpoint deployment

---
+++ src/database-postgres.ts
@@ -0,0 +1,80 @@
+import { randomUUID } from 'node:crypto'
+import sql from './db'
+
+export class DatabasePostgres {
+  async list(search?: string) {
+    let films
+
+    if (search) {
+      films = await sql`
+        SELECT * FROM films
+        WHERE name ILIKE ${'%' + search + '%'}
+        OR author ILIKE ${'%' + search + '%'}
+      `
+    } else {
+      films = await sql`SELECT * FROM films`
+    }
+
+    return films
+  }
+
+  async create(film: { name: string, author: string, description?: string, release_date?: string }) {
+    const filmId = randomUUID()
+    const { name, author, description, release_date } = film
+
+    await sql`
+      INSERT INTO films (id, name, author, description, release_date)
+      VALUES (${filmId}, ${name}, ${author}, ${description}, ${release_date})
+    `
+  }
+
+  async update(name: string, film: { name?: string, author?: string, description?: string, release_date?: string }) {
+    const fields: string[] = []
+    const values: any[] = []
+
+    if (film.name) {
+      fields.push(`name = $${fields.length + 1}`)
+      values.push(film.name)
+    }
+    if (film.author) {
+      fields.push(`author = $${fields.length + 1}`)
+      values.push(film.author)
+    }
+    if (film.description) {
+      fields.push(`description = $${fields.length + 1}`)
+      values.push(film.description)
+    }
+    if (film.release_date) {
+      fields.push(`release_date = $${fields.length + 1}`)
+      values.push(film.release_date)
+    }
+
+    if (fields.length === 0) return
+
+    values.push(name)
+
+    const query = `
+      UPDATE films
+      SET ${fields.join(', ')}
+      WHERE name = $${fields.length + 1}
+    `
+
+    await sql.unsafe(query, values)
+  }
+
+  async delete(name: string) {
+    await sql`
+      DELETE FROM films
+      WHERE name = ${name}
+    `
+  }
+
+  async findByName(name: string) {
+    const film = await sql`
+      SELECT * FROM films
+      WHERE name = ${name}
+    `
+    return film[0]
+  }
+}
+
--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,78 @@
 import fastify from "fastify"
 import sql from './db';
+import { DatabasePostgres } from "./database-postgres";
 
-const app = fastify()
+const server = fastify()
+const database = new DatabasePostgres()
 
-app.post('/film', () => {
-  return 'main'
+server.post('/film', async (request, reply) => {
+  const { name, author, description, release_date } = request.body as {
+    name: string
+    author: string,
+    description?: string,
+    release_date?: string
+  }
+
+  const filmAlreadyExist = await database.findByName(name)
+
+  if (filmAlreadyExist) {
+    return reply.status(400).send({ error: 'Film already exists.' })
+  }
+
+  await database.create({ name, author, description, release_date })
+
+  return reply.status(201).send({ message: 'Film created successfully.' })
 })
 
-app.get('/film', () => {
-  return 'create films'
+
+server.get('/film', async (request, reply) => {
+  const { search } = request.query as { search?: string }
+
+  const films = await database.list(search)
+
+  return reply.send(films)
 })
 
-app.put('/film/:name', () => {
-  return 'edit films'
+server.post('/film/:name', async (request, reply) => {
+  const { name, author, description, release_date } = request.body as {
+    name: string
+    author: string
+    description?: string
+    release_date?: string
+  }
+
+  const filmAlredyExist = await database.findByName(name)
+
+  if (filmAlredyExist) {
+    return reply.status(400).send({ error: 'Film already exists.' })
+  }
+
+  await database.create({ name, author, description, release_date })
+
+  return reply.status(201).send({ message: 'Film created successfully.' })
 })
 
-app.delete('/film/:name', () => {
-  return 'delete a film'
+server.delete('/film/:name', async (request, reply) => {
+  const { name } = request.params as { name: string }
+
+  const film = await database.findByName(name)
+
+  if (!film) {
+    return reply.status(404).send({ error: 'Film not found. ' })
+  }
+
+  await database.delete(name)
+
+  return reply.send({ message: 'Film deleted successfully ' })
 })
 
-app.get('/test-db', async () => {
+server.get('/test-db', async () => {
   const result = await sql`SELECT NOW()`;
   return { db_time: result[0].now };
 });
 
 
 
-app.listen({ port: 3000 }).then(() => {
+server.listen({ port: 3000 }).then(() => {
   console.log('HTTP server running.')
 })
